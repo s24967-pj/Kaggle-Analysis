bb506de2fda631429071d1b93a37307571945501
s24967-pj <s24967@pjwstk.edu.pl>
2024-06-12 02:04:17 +0200

    layout changes, adding scroll bar to the table

--- main.py
+++ main.py
@@ -20,19 +20,52 @@ class NetflixAnalysis:
     #Analiza Netflix dataset ladowanie danych i init dash app
     def __init__(self):
         self.netflix_df = self._load_data()
-        self.app = dash.Dash(__name__)
+        self.app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
         self.setup_layout()
     
     def setup_layout(self):
 
+        # self.app.layout = dbc.Container([
+        #     html.Div([
+        #     html.H1("Netflix Data Visualisation"), 
+        #     dcc.Graph(id='top-reviewed-movies', figure=self.plot_top_reviewed_movies()),
+        #     dcc.Graph(id='top-genres-movies', figure=self.plot_top_genres_movies())             
+        #     ]), 
+        #     self.table_top_reviewed()],   
+        # )
+
         self.app.layout = dbc.Container([
-            html.Div([
-            html.H1("Netflix Data Visualisation"), 
-            dcc.Graph(id='top-reviewed-movies', figure=self.plot_top_reviewed_movies()),
-            dcc.Graph(id='top-genres-movies', figure=self.plot_top_genres_movies())             
-            ]), 
-            self.table_top_reviewed()]      
-        )
+                dbc.Row(
+                        dbc.Col(
+                            [html.H1("Netflix Data Visualisation", style={'fontSize': 60, 'textAlign': 'center', 'color': '#800080'})],
+                            width={'size': 6, 'offset': 3},
+                        ),
+                ),
+                dbc.Row(
+                    # [
+                        dbc.Col(
+                            dcc.Graph(id='top-reviewed-movies', figure=self.plot_top_reviewed_movies())
+                        )
+                    #     dbc.Col(
+                    #         [self.table_top_reviewed()],
+                    #         width={'size': 5, 'order': 2, 'offset': 1},
+                    #     )
+                    # ]
+                ),
+                dbc.Row(
+                        dbc.Col(
+                            dcc.Graph(id='top-genres-movies', figure=self.plot_top_genres_movies())
+                        )
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [self.table_top_reviewed()],
+                            width={'size': 6, 'offset': 3}
+                        )
+                    ]
+                )
+        ])
        
 
     def setup_callbacks(self):
@@ -44,8 +77,8 @@ class NetflixAnalysis:
         #Wykres najlepiej ocenianych filmow
         top_reviewed = self.netflix_df.drop_duplicates('title', keep='first').nlargest(10, "rating")[["title", "rating"]] #wybor 10 filmow z najwieksza wartoscia w 'rating' zapisujac 1 wynik, aby nie bylo powtorek
         colors = ['#ff0087']
-        fig = px.bar(top_reviewed, x='rating', y='title', orientation='h', title="Top 10 Best Reviewed Movies", color_discrete_sequence=colors)
-        fig.update_layout(xaxis_title='Rating', yaxis_title='Movie Title')
+        fig = px.bar(top_reviewed, x='rating', y='title', orientation='h',  color_discrete_sequence=colors)
+        fig.update_layout(xaxis_title='Rating', yaxis_title='Movie Title', title="Top 10 Best Reviewed Movies", title_font=dict(size=20, color='black', family='Arial', weight='bold'), title_x=0.5)
         return fig
 
     def plot_top_genres_movies(self):
@@ -67,14 +100,38 @@ class NetflixAnalysis:
         values = genre_counts.values #wartosci do wykresu
         colors = ['#ffe0ee', '#ffc2db', '#ffa9c7', '#ff679d', '#ff0087']
         fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5, marker=dict(colors=colors))])
-        fig.update_layout(title_text='Top 5 Popular Genres')
+        fig.update_layout(title_text='Top 5 Popular Genres', title_font=dict(size=20, color='black', family='Arial', weight='bold'), title_x=0.5)
         return fig
 
     def table_top_reviewed(self):
        
         review_sort = self.netflix_df.drop_duplicates('title', keep='first').sort_values('rating', ascending=False)[["title", "rating"]].head(50) #sortowanie 50 danych z ratings
       
-        table = dash_table.DataTable(review_sort.to_dict('records')) #zapisanie posortowanych danych do tabeli
+        
+        table = dash_table.DataTable(review_sort.to_dict('records'),
+            # {"field": "rating", "sort": "desc"},
+            style_header={
+                'backgroundColor': 'pink',
+                'fontWeight': 'bold'
+            },
+            style_data={
+                'whiteSpace': 'auto',
+                'height': 'auto'
+            },
+            style_table={
+                'height': '300px', 
+                'overflowY': 'auto'
+            },
+            style_cell={
+                'textAlign': 'left'
+            },
+            style_cell_conditional=[
+                {'if': {'column_id': 'title'},
+                 'width': 'auto'},
+                {'if': {'column_id': 'rating'},
+                  'width': '90px'}
+            ],      
+        ) #zapisanie posortowanych danych do tabeli
         return table
 
     def _update_dictionary(self, dictionary, genre):
