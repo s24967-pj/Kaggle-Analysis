9e591e86ee35b6f6e7d5bf17706c1986fcd5abd3
s24967-pj <s24967@pjwstk.edu.pl>
2024-06-13 21:50:50 +0200

    plot and table chnges

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-
 import dash
 import dash_ag_grid as dag
 import dash_bootstrap_components as dbc
@@ -17,9 +16,15 @@ class NetflixAnalysis:
 
     def __init__(self):
         self.netflix_df = self._load_data()
+        self._clean_df()
         self.app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
         self.setup_layout()
         self.setup_callbacks()
+
+    def _clean_df(self):
+        self.netflix_df.drop_duplicates('title', keep='first', inplace=True) #usuniecie duplikatow z df
+        self.netflix_df['genre'] = self.netflix_df['genre'].str.split(', ')
+        return
 
     def run_server(self):
         """Uruchomienie serwera Dash."""
@@ -28,7 +33,7 @@ class NetflixAnalysis:
     def setup_layout(self):
         """Ustawienie layoutu aplikacji."""
         self.app.layout = dbc.Container([self.create_header(), self.create_top_reviewed_section(), self.create_top_genres_section(),
-            self.create_input_section(), self.create_actor_movie_table_section()])
+        self.create_actor_movie_table_section()])
 
     @staticmethod
     def create_header():
@@ -54,17 +59,12 @@ class NetflixAnalysis:
         """Funkcja zwracajaca sekcje z wykresem najczesciej wystepujacych gatunkow filmow."""
         return dbc.Row(dbc.Col(dcc.Graph(id='top-genres-movies', figure=self.plot_top_genres_movies())))
 
-    @staticmethod
-    def create_input_section():
-        """Funkcja zwracajaca input do wprowadzania danych."""
-        return dbc.Row(dbc.Col([dcc.Input(id='input1', type='text', debounce=True, value=""), html.Div(id='output')]))
-
     def create_actor_movie_table_section(self):
         """Funkcja zwracajaca sekcje z tabela aktorow i filmow."""
         return dbc.Row([dbc.Col([dcc.Input(id='actor-input', type='text', placeholder='Enter actor name', debounce=True),
-            html.Div(id='actor-movie-table', children=self.create_actor_movie_table(self.netflix_df))])])
+            html.Div(id='actor-movie-table', children=self.create_actor_movie_table())])])
 
-    def create_actor_movie_table(self, df):
+    def create_actor_movie_table(self):
         """Funkcja zwracajaca tabele z aktorami i filmami."""
         columnDefs = [{
             'field': 'title',
@@ -73,9 +73,7 @@ class NetflixAnalysis:
             'field': 'stars',
             'headerName': 'Actors'
         }]
-        table = dag.AgGrid(rowData=df[['title', 'stars']].drop_duplicates().to_dict("records"), columnDefs=columnDefs, defaultColDef={
-            "filter": True,
-            "sortable": True
+        table = dag.AgGrid(rowData=self.netflix_df[['title', 'stars']].to_dict("records"), columnDefs=columnDefs, defaultColDef={
         }, columnSize="sizeToFit", dashGridOptions={
             "animateRows": False
         })
@@ -83,8 +81,9 @@ class NetflixAnalysis:
 
     def plot_top_reviewed_movies(self):
         """Funkcja zwracajaca wykres najlepiej ocenianych filmow."""
-        top_reviewed = self.netflix_df.drop_duplicates('title', keep='first').nlargest(10, "rating")[
+        top_reviewed = self.netflix_df.nlargest(10, "rating")[
             ["title", "rating"]]  # wybor 10 filmow z najwieksza wartoscia w 'rating' zapisujac 1 wynik, aby nie bylo powtorek
+        top_reviewed = top_reviewed.sort_values(by="rating", ascending=True)
         colors = ['#ff0087']
         fig = px.bar(top_reviewed, x='rating', y='title', orientation='h', color_discrete_sequence=colors)
         fig.update_layout(xaxis_title='Rating',
@@ -96,21 +95,11 @@ class NetflixAnalysis:
 
     def plot_top_genres_movies(self):
         """"Funkcja zwracajaca wykres najczesciej wystepujacych gatunkow filmow."""
-        dictionary = {}  # Stworzenie slownika
-
-        for genre in self.netflix_df["genre"]:  # Przechodzimy przez cala koumne genre
-            if type(genre) is not str:  # Rozwiaznie problemu zle wprowadzonych danych w jednym z filmow
-                continue
-            if ',' not in genre:  # Jeżeli w 'genre' nie ma kilku gatunkow przypisanych do jednego filmu
-                self._update_dictionary(dictionary, genre)
-            else:  # Jeżeli mamy przypisane wiecej niz jeden gatunek do filmu
-                for element in genre.split(', '):  # rozdzielanie gatunkow po przecinku
-                    self._update_dictionary(dictionary, element)
-
-        genre_counts_df = pd.DataFrame.from_dict(data=dictionary, orient='index', columns=['counts'])  # Zmiana typu z listy na dataframe
-        genre_counts = genre_counts_df['counts'].sort_values(ascending=False).head(5)  # Sortowanie wartosci malejaco i wybranie 5
+        # genre_counts = self.genre_counts_df['counts'].sort_values(ascending=False).head(5)  # Sortowanie wartosci malejaco i wybranie 5
+        df_genres = self.netflix_df.explode('genre')
+        genre_counts = df_genres['genre'].value_counts().head(5)
         labels = genre_counts.index  # Etykiety
-        values = genre_counts.values  # Wartosci do wykresu
+        values = genre_counts  # Wartosci do wykresu
         colors = ['#ffe0ee', '#ffc2db', '#ffa9c7', '#ff679d', '#ff0087']
         fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5, marker=dict(colors=colors))])
         fig.update_layout(title_text='Top 5 Popular Genres', title_font=dict(size=20, color='black', family='Arial', weight='bold'), title_x=0.5)
@@ -118,7 +107,7 @@ class NetflixAnalysis:
 
     def table_top_reviewed(self):
         """Funkcja zwracajaca tabelę z najlepiej ocenianymi filmami."""
-        review_sort = self.netflix_df.drop_duplicates('title', keep='first')[["title", "rating"]]
+        review_sort = self.netflix_df[["title", "rating"]]
         columnDefs = [{
             'field': 'title',
             'sortable': False
@@ -135,17 +124,9 @@ class NetflixAnalysis:
 
     def setup_callbacks(self):
         """Ustawienie callbacków."""
-
-        @callback(Output("output", "children"), Input("input1", "value"))
-        def update_output(input1):
-            data = self.netflix_df.drop_duplicates('title', keep='first')[["title", "stars"]]
-            data2 = data[data['stars'].str.contains(input1, na=False)]
-            title = data2['title']
-            return f'Films: {title.values}'
-
         @callback(Output("actor-movie-table", "children"), Input("actor-input", "value"))
         def update_actor_movie_table(actor_name):
-            data = self.netflix_df.drop_duplicates('title', keep='first')[["title", "stars"]]
+            data = self.netflix_df[["title", "stars"]]
             if actor_name:
                 filtered_data = data[data['stars'].str.contains(actor_name, na=False)]
             else:
@@ -159,7 +140,7 @@ class NetflixAnalysis:
                 'headerName': 'Actors'
             }]
             table = dag.AgGrid(rowData=filtered_data.to_dict("records"), columnDefs=columnDefs, defaultColDef={
-                "filter": True,
+                "filter": False,
                 "sortable": True
             }, columnSize="sizeToFit", dashGridOptions={
                 "animateRows": False
