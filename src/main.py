6836a671e220680f3c36a9995a43b45b558e3bc3
s24967-pj <s24967@pjwstk.edu.pl>
2024-06-15 13:02:40 +0200

    last changes

--- src/main.py
+++ src/main.py
@@ -15,16 +15,20 @@ class NetflixAnalysis:
     """Analiza Netflix dataset ładowanie danych i init dash app."""
 
     def __init__(self):
-        self.netflix_df = self._load_data()
-        self._clean_df()
-        self.app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+        """Konstruktor."""
+        self.netflix_df = self._load_data()  # załadowanie danych
+        self._clean_df()  # funkcja do czyszczenia danych w pliku
+        self.app = dash.Dash(__name__, external_stylesheets=[
+                             dbc.themes.BOOTSTRAP])  # init bootstrapa
         self.setup_layout()
         self.setup_callbacks()
 
     def _clean_df(self):
-        self.netflix_df.drop_duplicates('title', keep='first', inplace=True) #usuniecie duplikatow z df
+        """Oczyszczanie pliku csv"""
+        self.netflix_df.drop_duplicates(
+            'title', keep='first', inplace=True)  # usuniecie duplikatow z df
+        # roroznienie danych zapisanych po przecinku w kolumnie genre
         self.netflix_df['genre'] = self.netflix_df['genre'].str.split(', ')
-        return
 
     def run_server(self):
         """Uruchomienie serwera Dash."""
@@ -33,10 +37,12 @@ class NetflixAnalysis:
     def setup_layout(self):
         """Ustawienie layoutu aplikacji."""
         self.app.layout = dbc.Container([self.create_header(), self.create_top_reviewed_section(), self.create_top_genres_section(),
-        self.create_actor_movie_table_section()])
+                                         # stworzenie containera z metodami tworzacymi widok kolumn i rzedow
+                                         self.create_actor_movie_table_section()])
 
     @staticmethod
     def create_header():
+        """Funkcja zwracajaca naglowek."""
         return dbc.Row(dbc.Col(html.H1("Netflix Data Visualisation", style={
             'fontSize': 50,
             'textAlign': 'center',
@@ -44,43 +50,39 @@ class NetflixAnalysis:
             'padding-bottom': '5rem',
             'padding-top': '3rem'
         }), width={
-            'size': 6,
-            'offset': 3
+            'size': 6,  # 6/12
+            'offset': 3  # odleglosc
         }))
 
     def create_top_reviewed_section(self):
         """Funkcja zwracajaca sekcje z wykresem najlepiej ocenianych filmow."""
         return dbc.Row([dbc.Col(dcc.Graph(id='top-reviewed-movies', figure=self.plot_top_reviewed_movies())),
-            dbc.Col(self.table_top_reviewed(), width={
-                'size': 4,
-                'order': 2
-            })])
+                        dbc.Col(self.table_top_reviewed(), width={
+                            'size': 4,
+                            'order': 2
+                        })])  # rzad zawierajacy dwie kolumny - z graphem i tabela
 
     def create_top_genres_section(self):
         """Funkcja zwracajaca sekcje z wykresem najczesciej wystepujacych gatunkow filmow."""
         return dbc.Row([
-        dbc.Col([
-            dbc.Button('Change values', outline=True, color="info", className="me-1", id='btn-nclicks-1', n_clicks=1)
-            # html.Div(id='plot-top-genres')
-        ], width={
-            'size': 1,
-            'offset': 2
-        }, style={'padding-top': 200}),
-        # dbc.Col(dcc.Graph(id='plot-top-genres', figure=self.plot_top_genres_movies()),
-        #     width={
-        #     'size': 6
-        # })
-        dbc.Col(html.Div(id='plot-top-genres', children=dcc.Graph(figure=self.plot_top_genres_movies())),
-            width={
-            'size': 6
-        })
-    ])
+            dbc.Col([
+                dbc.Button('Change values', outline=True, color="info",
+                       className="me-1", id='btn-nclicks-1', n_clicks=1)
+            ], width={
+                'size': 1,
+                'offset': 2
+            }, style={'padding-top': 200}),  # ustawienie przysisku na srodku
+            dbc.Col(html.Div(id='plot-top-genres', children=dcc.Graph(figure=self.plot_top_genres_movies())),
+                    width={
+                'size': 6
+            })
+        ])  # 1R 2K z przyciskiem i wykresem kolwoym
 
-    
     def create_actor_movie_table_section(self):
         """Funkcja zwracajaca sekcje z tabela aktorow i filmow."""
         return dbc.Row([dbc.Col([dcc.Input(id='actor-input', type='text', placeholder='Enter actor name', debounce=True),
-            html.Div(id='actor-movie-table', children=self.create_actor_movie_table())])])
+                                 # rzad z tabela aktorow i filmow, input do wprowadzania danych
+                                 html.Div(id='actor-movie-table', children=self.create_actor_movie_table())])])
 
     def create_actor_movie_table(self):
         """Funkcja zwracajaca tabele z aktorami i filmami."""
@@ -90,71 +92,77 @@ class NetflixAnalysis:
         }, {
             'field': 'stars',
             'headerName': 'Actors'
-        }]
+        }]  # zdefiniowanie kolumn tabeli, nadanie nazw
         table = dag.AgGrid(rowData=self.netflix_df[['title', 'stars']].to_dict("records"), columnDefs=columnDefs, defaultColDef={
         }, columnSize="sizeToFit", dashGridOptions={
             "animateRows": False
-        })
+        })  # tworzenie tabeli z danymi z pliku csv
         return table
 
     def plot_top_reviewed_movies(self):
         """Funkcja zwracajaca wykres najlepiej ocenianych filmow."""
         top_reviewed = self.netflix_df.nlargest(10, "rating")[
-            ["title", "rating"]]  # wybor 10 filmow z najwieksza wartoscia w 'rating' zapisujac 1 wynik, aby nie bylo powtorek
-        top_reviewed = top_reviewed.sort_values(by="rating", ascending=True)
+            ["title", "rating"]]  # wybor 10 filmow z najwieksza wartoscia w 'rating'
+        top_reviewed = top_reviewed.sort_values(
+            by="rating", ascending=True)  # sortowanie danych malejaco
         colors = ['#ff0087']
-        fig = px.bar(top_reviewed, x='rating', y='title', orientation='h', color_discrete_sequence=colors)
+        fig = px.bar(top_reviewed, x='rating', y='title',
+                     orientation='h', color_discrete_sequence=colors)
         fig.update_layout(xaxis_title='Rating',
                           yaxis_title='Movie Title',
                           title="Top 10 Best Reviewed Movies",
-                          title_font=dict(size=20, color='#89CFF0', family='Arial', weight='bold'),
+                          title_font=dict(size=20, color='#89CFF0',
+                                          family='Arial', weight='bold'),
                           title_x=0.5)
         return fig
 
     def plot_top_genres_movies(self):
         """"Funkcja zwracajaca wykres najczesciej wystepujacych gatunkow filmow."""
-        # genre_counts = self.genre_counts_df['counts'].sort_values(ascending=False).head(5)  # Sortowanie wartosci malejaco i wybranie 5
-        df_genres = self.netflix_df.explode('genre')
-        genre_counts = df_genres['genre'].value_counts().head(5)
+        df_genres = self.netflix_df.explode(
+            'genre')  # rozdzielenie gatunkow na osobne rzedy - kilka do jednego filmu
+        genre_counts = df_genres['genre'].value_counts().head(
+            5)  # zliczenie 5 najczestszych
         labels = genre_counts.index  # Etykiety
         values = genre_counts  # Wartosci do wykresu
         colors = ['#ffe0ee', '#ffc2db', '#ffa9c7', '#ff679d', '#ff0087']
-        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.5, marker=dict(colors=colors))])
-        fig.update_layout(title_text='Top 5 Popular Genres', title_font=dict(size=20, color='#89CFF0', family='Arial', weight='bold'), title_x=0.5)     
+        fig = go.Figure(
+            data=[go.Pie(labels=labels, values=values, hole=.5, marker=dict(colors=colors))])
+        fig.update_layout(title_text='Top 5 Popular Genres', title_font=dict(
+            size=20, color='#89CFF0', family='Arial', weight='bold'), title_x=0.5)
         return fig
 
     def table_top_reviewed(self):
         """Funkcja zwracajaca tabelę z najlepiej ocenianymi filmami."""
-        review_sort = self.netflix_df[["title", "rating"]]
+        review_sort = self.netflix_df[["title", "rating"]
+                                      ]  # z csv bierzemy 2 kolumy -title i rating
         columnDefs = [{
             'field': 'title',
             'sortable': False
         }, {
             'field': 'rating'
-        }]
-
+        }]  # zdef kolumn
         table = html.Div([dag.AgGrid(id="row-sorting-simple", rowData=review_sort.to_dict("records"), columnDefs=columnDefs, defaultColDef={
             "filter": True
         }, columnSize="sizeToFit", dashGridOptions={
             "animateRows": False
+            # tworzymy tabele, todict aby dane byly przekazane w odpowiedni sposob 'records’ : list like [{column -> value},{tytul -> Avatar}] - zwracamy liste slownikow
         })])
         return table
-    
-    def hover(self):
-        fig = fig.update_traces(hoverinfo='label+percent', textinfo='value')
-        
-
 
     def setup_callbacks(self):
         """ustawienie callbackow"""
-        @callback(
+        @callback(  # eventy do odbierania zdarzen, aby akutalizowac aplikacje, aby apka byla responsywna
+            # id i children jako komponent ktory chce sae zaaktualizowac
             Output("actor-movie-table", "children"),
+            # id ktory nasluchujemy na zdarzenie i value jako wartosc przekazywana do metody i wykorzystywana do przefiltrowania danych
             Input("actor-input", "value")
         )
         def update_actor_movie_table(actor_name):
             data = self.netflix_df[["title", "stars"]]
             if actor_name:
-                filtered_data = data[data['stars'].str.contains(actor_name, na=False)]
+                # sprawdzamy czy zostala wpisana wartosc i czy kolumna ja zawiera
+                filtered_data = data[data['stars'].str.contains(
+                    actor_name, na=False)]
             else:
                 filtered_data = data
 
@@ -170,10 +178,10 @@ class NetflixAnalysis:
                 "sortable": True
             }, columnSize="sizeToFit", dashGridOptions={
                 "animateRows": False
-            })
+            })  # zwracane przefiltrowane dane w tabeli
 
-            return table
-        
+            return table  # zwracana jest nowa zupdatowana do naszych wyszukiwan tabela
+
         @callback(
             Output('plot-top-genres', 'children'),
             Input('btn-nclicks-1', 'n_clicks')
@@ -181,18 +189,11 @@ class NetflixAnalysis:
         def update_pie_chart(n_clicks):
             fig = self.plot_top_genres_movies()
             if n_clicks % 2 == 0:
+                # jesli modulo z liczby klikniec rowna sie 0 pokazujemy dane bez %
                 return dcc.Graph(figure=fig.update_traces(hoverinfo='label+percent', textinfo='value'))
             else:
+                # jak modulo inne to graf sie nie zmienia
                 return dcc.Graph(figure=fig)
-            
-    
-    @staticmethod
-    def _update_dictionary(dictionary, genre):
-        """Funkcja pomocnicza do tworzenia slownika z gatunkami filmow."""
-        if genre in dictionary:  # Jezeli gatunek jest juz zapisany w slowniczku dodajemy licznik +1, w przeciwnym przypadku zapisujemy w slowniku i ustawiamy licznik na 1
-            dictionary[genre] = dictionary[genre] + 1
-        else:
-            dictionary[genre] = 1
 
     @staticmethod
     def _load_data():
